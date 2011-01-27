ca1a7e09a8d11b152ff95c409e42666c39b62e35
Stefan Goodchild <me@stefangoodchild.com>
2011-01-27 15:21:49 +0000

    Markup for single column page.

--- onecolumn-page.php
+++ onecolumn-page.php
@@ -16,12 +16,21 @@ get_header(); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-				<h1><?php the_title(); ?></h1>
-				<?php the_content(); ?>
-				<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
-
-				<?php comments_template( '', true ); ?>
+					<article>
+						<?php if ( is_front_page() ) { ?>
+							<h2><?php the_title(); ?></h2>
+						<?php } else { ?>	
+							<h1><?php the_title(); ?></h1>
+						<?php } ?>
+						<section class="content">
+						<?php the_content(); ?>
+						</section>
+						<footer class="article">
+							<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
+							<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
+						</footer>
+						<?php comments_template( '', true ); ?>
+					</article>
 
 <?php endwhile; ?>
 
