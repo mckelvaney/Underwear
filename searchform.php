48fd8315cdeb92b02e64f54c293052b27c464b02
Stefan Goodchild <me@stefangoodchild.com>
2011-01-27 15:06:15 +0000

    Search form now respects the vertical rhythm

---
+++ searchform.php
@@ -0,0 +1,11 @@
+<form role="search" method="get" id="searchform" action="http://localhost:8888/">
+	<ol>
+		<li>
+			<label class="screen-reader-text" for="s">Search for</label>
+			<input type="text" value="" name="s" id="s">
+		</li>
+		<li>
+		<button type="submit" id="searchsubmit">Search</button>
+		</li>
+	</ol>
+</form>
