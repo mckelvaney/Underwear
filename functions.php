deeb1fe0f4956b38c45a317d0348fb4d8075ee08
Stefan Goodchild <me@stefangoodchild.com>
2011-01-27 14:35:57 +0000

    Small tweak to comment list function and style

--- functions.php
+++ functions.php
@@ -317,12 +317,11 @@ function twentyten_comment( $comment, $args, $depth ) {
 	switch ( $comment->comment_type ) :
 		case '' :
 	?>
-	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-		<div id="comment-<?php comment_ID(); ?>">
+	<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
 		<div class="comment-author vcard">
 			<?php echo get_avatar( $comment, 40 ); ?>
 			<?php printf( __( '%s <span class="says">says:</span>', 'twentyten' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
-		</div><!-- .comment-author .vcard -->
+		</div>
 		<?php if ( $comment->comment_approved == '0' ) : ?>
 			<em><?php _e( 'Your comment is awaiting moderation.', 'twentyten' ); ?></em>
 			<br />
@@ -333,15 +332,13 @@ function twentyten_comment( $comment, $args, $depth ) {
 				/* translators: 1: date, 2: time */
 				printf( __( '%1$s at %2$s', 'twentyten' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'twentyten' ), ' ' );
 			?>
-		</div><!-- .comment-meta .commentmetadata -->
+		</div>
 
 		<div class="comment-body"><?php comment_text(); ?></div>
 
 		<div class="reply">
 			<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-		</div><!-- .reply -->
-	</div><!-- #comment-##  -->
-
+		</div>
 	<?php
 			break;
 		case 'pingback'  :
